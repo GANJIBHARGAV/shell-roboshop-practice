1249b167faacc0870f17bf00f2a879782f4634b7
Bhargav Ganji <bhargavganji08.com>
2025-05-23 12:12:10 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -11,7 +11,7 @@ DOMAIN_ID="bhargavcommerce.shop"
 for instance in ${INSTANCES[@]}
 do
 INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-061c4e443b6a8969b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-if [ instance -ne "frontend" ]
+if [ instance != "frontend" ]
 then
 IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 else
