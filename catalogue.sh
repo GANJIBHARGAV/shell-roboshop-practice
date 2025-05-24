918ee2760472062cd44a0ad9358c0d80af9809db
Bhargav Ganji <bhargavganji08.com>
2025-05-25 04:40:04 +0530

    roboshop started

--- catalogue.sh
+++ catalogue.sh
@@ -32,8 +32,8 @@ dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enabling the nodejs"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing the nodejs"
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-VALIDATE $? "validating the system user"
+#useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+#VALIDATE $? "validating the system user"
 # creating the app directory
 mkdir /app
 VALIDATE $? "creating the app directory"
