1c32a18c1f54e6020a18daf38c29720f5d845434
Bhargav Ganji <bhargavganji08.com>
2025-05-25 04:22:34 +0530

    roboshop started

---
+++ catalogue.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+userid=$(id -u)
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_FILE.log"
+SCRIPT_DIR=$PWD
+
+
+mkdir -p $LOGS_FOLDER
+echo "Script execution started at $(date)" | tee -a $LOG_FILE
+if [ $userid -ne 0 ]
+then
+echo "Error:: please run with root user" | tee -a $LOG_FILE
+exit 1
+else
+echo "Super.. under the root user only" | tee -a $LOG_FILE
+fi
+# validate function is used to take input1 as exit status and input2 as server as $1 ans $2
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then
+echo "$2 ... SUCCESS" | tee -a $LOG_FILE
+else
+echo "$2 ... FAILED" | tee -a $LOG_FILE
+exit 1
+fi
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling the nodejs"
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling the nodejs"
+dnf install nodejs -y
+VALIDATE $? "Installing the nodejs"
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "validating the system user"
+# creating the app directory
+mkdir /app
+VALIDATE $? "creating the app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "downloading the catalogue code in zip mode"
+cd /app 
+unzip /tmp/catalogue.zip
+VALIDATE $? "unzipping the code"
+npm install
+VALIDATE $? "Installing the node package manager"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+VALIDATE $? "Daemon reload catalogue service"
+
+systemctl enable catalogue 
+VALIDATE $? "enabling the catalogue"
+systemctl start catalogue
+VALIDATE $? "starting the catalogue"
+
+
